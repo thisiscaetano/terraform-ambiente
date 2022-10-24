e8dede78a5414a693a7c1a1bda1a4daf11d083a3
thisiscaetano <juncaetano@icloud.com>
2022-10-24 11:44:13 -0300

    First commit use modules ec2

---
+++ files/userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+amazon-linux-extras install nginx -y
+systemctl enable nginx
+systemctl start nginx
---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = ">= 4.0"
+  }
+  backend "s3" {
+    bucket = "admin-terraform-curso-state"
+    key    = "terraform-ambiente.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  profile                  = "default"
+  shared_credentials_files = ["~/.aws/credentials"]
+  region                   = "us-east-1"
+}
+
+module "ec2" {
+  source    = "git@github.com:thisiscaetano/terraform-aws-ec2.git"
+  int_type  = "t2.micro"
+  int_name  = "web"
+  user_data = file("./files/userdata.sh")
+  amis      = "ami-09d3b3274b6c5d4aa"
+}
